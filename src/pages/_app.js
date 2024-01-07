28e5f73dee1907f243d78b67a64bd0470e6c217c
ribhavj <ribhavjain9@gmail.com>
2024-01-07 21:14:15 +0530

    Portfolio

---
+++ src/pages/_app.js
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { ThemeProvider } from "styled-components";
+import TagManager from "react-gtm-module";
+
+import { chosenTheme } from "../styles/theme";
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const tagManagerArgs = {
+        gtmId: process.env.NEXT_PUBLIC_GTM_TRACKING_ID,
+      };
+      TagManager.initialize(tagManagerArgs);
+    }
+  }, []);
+
+  return (
+    <ThemeProvider theme={chosenTheme}>
+      <>
+        <div>
+          <Component {...pageProps} />
+        </div>
+      </>
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
---
+++ src/styles/theme.js
@@ -0,0 +1,33 @@
+// Themes
+
+export const lightTheme = {
+  body: "#FFF",
+  text: "#292F33",
+  secondaryText: "#5F6368",
+  imageHighlight: "#0E6BA8",
+  buttonColor: "#0A66C2",
+  hoverButtonColor: "#084d92",
+  headerColor: "#C0C0C0",
+  highlight: "#EEEEEE",
+  dark: "#202124",
+  compImgHighlight: "#E1D9D1",
+  jacketColor: "#0A2472",
+  CompImgColor: "#292F33",
+};
+
+export const darkTheme = {
+  body: "#121212",
+  text: "#FFFFFFFF",
+  secondaryText: "#FFFFFFb3",
+  buttonColor: "#FFFFFFF2",
+  hoverButtonColor: "#FFFFFFdb",
+  imageHighlight: "#3700b3",
+  headerColor: "#FF990070",
+  highlight: "#2E2E2E",
+  dark: "#000000",
+  compImgHighlight: "#FFFFFF1a",
+  jacketColor: "#8d99ae",
+  CompImgColor: "#191919",
+};
+
+export const chosenTheme = lightTheme;
